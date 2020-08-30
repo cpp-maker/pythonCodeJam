68c65ac1a98067db7b3f63de9f927c6f6394e4e4
cpp-maker <t.frankel@outlook.com>
2020-08-30 22:13:42 +0100

    fixed format issue

--- codeJamGui/pyjamGen.py
+++ codeJamGui/pyjamGen.py
@@ -1,14 +1,3 @@
-<<<<<<< HEAD:codeJamGui/pyjamGen.py
-import random
-import string 
-
-
-def gen_code(length):
-	return ("".join(random.choice(string.ascii_letters + string.digits) for _ in range(length)))
-
-
-print(gen_code(5))
-=======
 #Copyright (C) 2020 author name here
 #This program is free software: you can redistribute it and/or modify
 #it under the terms of the GNU General Public License as published by
@@ -32,4 +21,4 @@ def gen_code(length):
 
 
 print(gen_code(5))
->>>>>>> 970b1cbdd27d0feae658f1d939d95fffdb10b512:pyjamGen.py
+
