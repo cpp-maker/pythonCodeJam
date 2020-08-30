63cb82fd6c2ab7ffa91374710c33cddee2d171c5
0xReflex <prrprrbro23@gmail.com>
2020-08-30 18:54:35 +0530

    under work

--- decode.py
+++ decode.py
@@ -1,20 +1,16 @@
-from string import printable
+import string
 import random
-import time
 
-a = time.time()
-digit = printable.strip()
-#print(digit)
-lenght = input("Password Lenght ")
-if lenght == '':
-	print("no password is of 0 words")
-	exit()
-elif lenght.replace(' ','').isalpha() == True:
-	print("type a number")
-	exit()
-else:
-	passw  = []
-	for i in range(int(lenght)):
-		passw.append(random.choice(digit))
-word = ''.join(passw)
-print(word)
+
+pass_length  = input()#length of password
+def pass_gen(lenght):
+	if lenght == '':# to check if password have any digit
+		print("no password is of 0 words")
+		exit()
+	elif lenght.replace(' ','').isalpha() == True:# to check if it is not a 
+		print("type a number")
+		exit()
+	else:
+		passwordd = ("".join(random.choice(string.printable.strip())for _ in range(int(lenght))))
+		return passwordd
+print(pass_gen(pass_length))
